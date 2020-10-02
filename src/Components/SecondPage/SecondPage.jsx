0d1c5753d2b6c7a8797f2d446aea4bc481f1ed9b
torstein_mp <p98avlo@gmail.com>
2020-10-02 14:01:43 +0300

    fixed displayName of undefined

--- src/Components/SecondPage/SecondPage.jsx
+++ src/Components/SecondPage/SecondPage.jsx
@@ -4,7 +4,7 @@ import {connect} from "react-redux";
 export const Page = ({chosenCity}) => {
     return (
         <div>
-            {chosenCity.displayName || ""}
+            {chosenCity?.displayName || ""}
         </div>
     );
 
